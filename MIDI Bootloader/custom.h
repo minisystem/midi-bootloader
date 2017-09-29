07271a21a7c211637245a84f1b58ee7e09a5eb17
minisystem <jeffrey.lee@gmail.com>
2017-09-28 22:40:28 -0400

    first commit

---
+++ MIDI Bootloader/custom.h
@@ -0,0 +1,24 @@
+/*
+ * Custom for MIDI Bootloader
+ *
+ * Created: 2017-09-26 12:35:45 PM
+ *  Author: jeff
+ */ 
+
+
+#ifndef INCFILE1_H_
+#define INCFILE1_H_
+
+//define switch pins
+#define START_STOP		PB1
+
+//define switch port
+#define SWITCH_PORT   PINB
+
+//define LED pins
+#define Y_LED		PE2
+#define R_LED		PC2
+
+
+
+#endif /* INCFILE1_H_ */
